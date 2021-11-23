8cf6c17b22e688058f26304ac129b84c4a834909
aleksandergs <alekrickrock@hotmail.com>
2021-11-23 13:26:01 -0400

    Funciones de busqueda en main y llenar la clase vacuna

--- Clinica/src/logic/Clinica.java
+++ Clinica/src/logic/Clinica.java
@@ -1,6 +1,7 @@
 package logic;
 
 import java.util.ArrayList;
+import java.util.Date;
 
 public class Clinica {
 
@@ -15,11 +16,109 @@ public class Clinica {
 		super();
 		misPacientes = new ArrayList<>();
 		misVacunas = new ArrayList<>();
+		misUsuarios = new ArrayList<>();
+		misEnfermedades = new ArrayList<>();
+		misCitas = new ArrayList<>();
 	}
 	
 	public static Clinica getInstance() {
 		if(clinica == null)
 			clinica = new Clinica();
 		return clinica;
+	}
+	
+	public void insertarPaciente(Paciente p) {
+		misPacientes.add(p);
+	}
+	
+	public void insertarVacuna(Vacuna v) {
+		misVacunas.add(v);
+	}
+
+	public void insertarUsuario(Usuario u) {
+		misUsuarios.add(u);
+	}
+	
+	public void insertarEnfermedad(Enfermedad e) {
+		misEnfermedades.add(e);
+	}
+	
+	public void insertarCita(CitaMedica c) {
+		misCitas.add(c);
+	}
+	
+	public Paciente buscarPaciente(String cedula)
+	{
+		Paciente paciente = null;
+		boolean encontrado = false;
+		int i = 0;
+		while (!encontrado && i < misPacientes.size()) {
+			if(misPacientes.get(i).getCedula().equalsIgnoreCase(cedula)) {
+				encontrado=true;
+				paciente = misPacientes.get(i);
+			}
+			i++;
+		}
+		return paciente;
+	}
+	
+	public Usuario buscarUsuario(String codigo)
+	{
+		Usuario usuario = null;
+		boolean encontrado = false;
+		int i = 0;
+		while (!encontrado && i < misUsuarios.size()) {
+			if(misUsuarios.get(i).getCodigoUsuario().equalsIgnoreCase(codigo)) {
+				encontrado=true;
+				usuario = misUsuarios.get(i);
+			}
+			i++;
+		}
+		return usuario;
+	}
+	
+	public Enfermedad buscarEnfermedad(String codigo)
+	{
+		Enfermedad enfermedad = null;
+		boolean encontrado = false;
+		int i = 0;
+		while (!encontrado && i < misEnfermedades.size()) {
+			if(misEnfermedades.get(i).getCodigo().equalsIgnoreCase(codigo)) {
+				encontrado=true;
+				enfermedad = misEnfermedades.get(i);
+			}
+			i++;
+		}
+		return enfermedad;
+	}
+	
+	public CitaMedica buscarCita(String codigo)
+	{
+		CitaMedica cita = null;
+		boolean encontrado = false;
+		int i = 0;
+		while (!encontrado && i < misCitas.size()) {
+			if(misCitas.get(i).getCodigo().equalsIgnoreCase(codigo)) {
+				encontrado=true;
+				cita = misCitas.get(i);
+			}
+			i++;
+		}
+		return cita;
+	}
+	
+	public Vacuna buscarVacuna(String codigo)
+	{
+		Vacuna vacuna = null;
+		boolean encontrado = false;
+		int i = 0;
+		while (!encontrado && i < misVacunas.size()) {
+			if(misVacunas.get(i).getCodigo().equalsIgnoreCase(codigo)) {
+				encontrado=true;
+				vacuna = misVacunas.get(i);
+			}
+			i++;
+		}
+		return vacuna;
 	}
 }
--- Clinica/src/logic/Vacuna.java
+++ Clinica/src/logic/Vacuna.java
@@ -2,8 +2,43 @@ package logic;
 
 public class Vacuna {
 
-	public Vacuna() {
-		// TODO Auto-generated constructor stub
+	private String codigo;
+	private String nombre;
+	int dosis;
+	private String fabricante;
+	
+	public Vacuna(String codigo, String nombre, int dosis, String fabricante) {
+		super();
+		this.codigo = codigo;
+		this.nombre = nombre;
+		this.dosis = dosis;
+		this.fabricante = fabricante;
+	}
+	
+	public String getCodigo() {
+		return codigo;
+	}
+	public void setCodigo(String codigo) {
+		this.codigo = codigo;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public int getDosis() {
+		return dosis;
+	}
+	public void setDosis(int dosis) {
+		this.dosis = dosis;
+	}
+	public String getFabricante() {
+		return fabricante;
+	}
+	public void setFabricante(String fabricante) {
+		this.fabricante = fabricante;
 	}
 
+	
 }
